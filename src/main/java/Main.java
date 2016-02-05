39963214af165b46b0892536387867e45eff324c
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-02-05 21:52:30 +0300

    Slides day 02

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * Created by eugene on 05.02.16.
+ */
+public class Main {
+    public static void main(String[] args) {
+        MyPrinter printer = new MyPrinter();
+        printer.print("Hello World!");
+    }
+}
