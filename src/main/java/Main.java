bcd14111731f8a59195a45904c57ca31079bd561
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2016-02-10 19:42:22 +0300

    Delete dummy example

--- src/main/java/Main.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by eugene on 05.02.16.
- */
-public class Main {
-    public static void main(String[] args) {
-        MyPrinter printer = new MyPrinter();
-        printer.print("Hello World!");
-    }
-}
