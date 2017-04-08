93d85871bd8143ccad3f3bd8e5ce96ce340a4a9e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2017-04-08 19:15:42 +0300

    Refactor to global state.

--- src/main/java/demo/Demo.java
+++ src/main/java/demo/Demo.java
@@ -2,18 +2,20 @@ package demo;
 
 public class Demo {
     public static void main(String[] args) {
-        Printer printer;
+        Printer
+                printer1 = new Printer("1"),
+                printer2 = new Printer("2");
 
-        printer = new Printer("HW!");
-        printer.print();
+        printer1.print();
+        printer2.print(); //Surprise!
     }
 }
 
 class Printer {
-    private String message; //Introduce object state with field/property
+    private static String message = "0"; //Introduce global state with static field
 
     Printer(String message) {
-        this.message = message;
+        this.message = message; //Error-prone approach
     }
 
     public void print() {
