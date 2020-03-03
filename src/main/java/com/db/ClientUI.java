15e65994d36b767ad90c76b5c1661c36c9b67e7f
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-03-03 14:12:36 +0300

    Comments demo.

--- src/main/java/com/db/ClientUI.java
+++ src/main/java/com/db/ClientUI.java
@@ -5,9 +5,24 @@ import com.db.service.Controller;
 
 import java.sql.SQLException;
 
+/**
+ * Text.
+ * @author
+ * @since 1.4
+ */
 public class ClientUI {
-    public static void main(String[] args ) throws BuyOperationException, SQLException {
+    /**
+     * Method info.
+     * @param args Why?
+     * @return what??
+     * @throws BuyOperationException
+     * @throws SQLException
+     * @since 1.4
+     */
+    public static void main(String[] args) throws BuyOperationException, SQLException {
         Controller controller = new Controller();
         controller.buy("GZP", 10);
+
+        System.out.println();
     }
 }
--- src/main/java/com/db/service/Controller.java
+++ src/main/java/com/db/service/Controller.java
@@ -7,12 +7,29 @@ import com.db.domain.Ticker;
 
 import java.sql.SQLException;
 
+
 public class Controller {
     private TickerDAO tickerDAO = new TickerDAO();
     private StockDAO stockDAO;
 
+    /**
+     * Byu ticker.
+     * @return ????
+     *
+     * 1. Side effects
+     * 2. Pre-conditions: params
+     * 3. Post-conditions: return
+     *
+     */
     public double buy(String tickerId, int amount) throws BuyOperationException, SQLException {
         final int BUY_OPERATION = 1;
+        Ticker tickerObject = getTickerFromDb(tickerId);
+
+        final String stringTicketRepresenation = tickerObject.toString();
+        return stockDAO.placeOrder(stringTicketRepresenation, amount, BUY_OPERATION);
+    }
+
+    private Ticker getTickerFromDb(String tickerId) throws SQLException, BuyOperationException {
         Ticker tickerObject = null;
 
         try {
@@ -24,8 +41,7 @@ public class Controller {
         } finally {
             tickerDAO.close();
         }
-
-        return stockDAO.placeOrder(tickerObject.toString(), amount, BUY_OPERATION);
+        return tickerObject;
     }
 
     public double sell(String tickerId, int amount) throws SellOperationException, SQLException {
