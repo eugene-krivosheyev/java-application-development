f85270ae09df7cb2bd6caa668d68d6ffe88ba5db
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-03-03 11:32:14 +0300

    Packages and Import Demo.

--- src/main/java/demo/Façade.java
+++ src/main/java/demo/Façade.java
@@ -1,5 +1,18 @@
 package demo;
 
+import java.lang.*;
+
+public class Façade {
+    public static final int MY_CONST = 0;
+    public static int objectsCount = 0;
+
+    public static int incrementCount(final int step) {
+        int localVar = 0;
+        java.lang.System.out.println(localVar);
+        return 0;
+    }
+}
+
 class C2 {
 }
 
