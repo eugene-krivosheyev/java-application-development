adc46197cdb5be8e3ed81f7b7ae58a3311cac97f
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-03-05 10:55:31 +0300

    Add literals demo.

---
+++ src/main/java/demo/LiteralsDemo.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class LiteralsDemo {
+    public static void main(String[] args) {
+        System.out.println("string literal");
+        System.out.println(new int[] {1,2});
+    }
+}
