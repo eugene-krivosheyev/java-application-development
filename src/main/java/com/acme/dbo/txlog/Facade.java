51969b86bfd39e8d9afb14712d03ec1573e9190e
dmitry-khleborodov <dmitry.khleborodov@gmail.com>
2020-03-10 14:36:54 +0300

    made Facade.log overloads

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -8,4 +8,20 @@ public class Facade {
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
+
+    public static void log(char message) {
+        System.out.println(String.format("char: %s", message));
+    }
+
+    public static void log(String message) {
+        System.out.println(String.format("string: %s", message));
+    }
+
+    public static void log(Boolean message) {
+        System.out.println(String.format("primitive: %s", message));
+    }
+
+    public static void log(Object message) {
+        System.out.println(String.format("reference: @"));
+    }
 }
--- src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
@@ -32,7 +32,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutEquals("primitive: 1\r\nprimitive: 0\r\nprimitive: -1\r\n");
         //endregion
     }
 
@@ -51,9 +51,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("-1");
         //endregion
     }
-
-    /*
-    TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogChar() throws IOException {
@@ -108,6 +105,4 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("@");
         //endregion
     }
-
-    */
 }
