5c2c18c92ce817f75e9323b1cae1ba64644a472f
dmitry-khleborodov <dmitry.khleborodov@gmail.com>
2020-03-10 15:26:47 +0300

    refactoring to constants and inversion of printer and formatter usage

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,35 +1,41 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    private static void print(String type, String message) {
-        System.out.println(format(type, message));
+
+    private static final String PREFIX_REFERENCE = "reference";
+    private static final String PREFIX_PRIMITIVE = "primitive";
+    private static final String PREFIX_STRING = "string";
+    private static final String PREFIX_CHAR = "char";
+
+    private static void printToConsole(String message) {
+        System.out.println(message);
     }
 
-    private static String format(String ...input) {
-        return String.format("%s: %s", input[0], input[1]);
+    private static String formatAsPrefixColonMessage(String prefix, String message) {
+        return String.format("%s: %s", prefix, message);
     }
 
     public static void log(int message) {
-        print("primitive", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_PRIMITIVE, String.valueOf(message)));
     }
 
     public static void log(byte message) {
-        print("primitive", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_PRIMITIVE, String.valueOf(message)));
     }
 
     public static void log(char message) {
-        print("char", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_CHAR, String.valueOf(message)));
     }
 
     public static void log(String message) {
-        print("string", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_STRING, String.valueOf(message)));
     }
 
     public static void log(Boolean message) {
-        print("primitive", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_PRIMITIVE, String.valueOf(message)));
     }
 
     public static void log(Object message) {
-        print("reference", String.valueOf(message));
+        printToConsole(formatAsPrefixColonMessage(PREFIX_REFERENCE, String.valueOf(message)));
     }
 }
