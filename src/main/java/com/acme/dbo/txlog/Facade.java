172c7d08f2433a5c3bd63427edfb3db3a63512fd
Vlad <mclaren04@yandex.ru>
2020-03-10 14:36:00 +0300

    Implement log for different types

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,10 +2,30 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        write("primitive", String.valueOf(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        write("primitive", String.valueOf(message));
+    }
+
+    public static void log(boolean message){
+        write("primitive", String.valueOf(message));;
+    }
+
+    public static void log(Object message){
+        write("reference", String.valueOf(message));
+    }
+
+    public static void log(char message){
+        write("char", String.valueOf(message));
+    }
+
+    public static void log(String message){
+        write("string", String.valueOf(message));
+    }
+
+    private static void write(String prefix, String payload) {
+        System.out.println(prefix + ": " + payload);
     }
 }
