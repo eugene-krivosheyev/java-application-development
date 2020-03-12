46806c8f8cc4558ec732f35ba9a06b16dd495b25
Artem Nikitin <artemnikotin@gmail.com>
2020-03-12 02:23:07 -0700

    implemented Facade due to requirements described in test

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,37 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+    private static String PRIMITIVE_PREFIX = "primitive";
+
+    public static void log(int i) {
+        print(formatWithPrefix(PRIMITIVE_PREFIX, String.valueOf(i)));
     }
 
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    public static void log(byte b) {
+        print(formatWithPrefix(PRIMITIVE_PREFIX, String.valueOf(b)));
+    }
+
+    public static void log(boolean b) {
+        print(formatWithPrefix(PRIMITIVE_PREFIX, String.valueOf(b)));
+    }
+
+    public static void log(char c) {
+        print(formatWithPrefix("char", String.valueOf(c)));
+    }
+
+    public static void log(String s) {
+        print(formatWithPrefix("string", s));
+    }
+
+    public static void log(Object o) {
+        print(formatWithPrefix("reference", String.valueOf(o)));
+    }
+
+    private static String formatWithPrefix(String prefix, String subject) {
+        return String.format("%s: %s", prefix, subject);
+    }
+
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
