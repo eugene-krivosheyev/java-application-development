1446efb12ce9f3f1a41f4a27c5b6c5cb05f76afe
S C <semitsvetik@gmail.com>
2020-03-18 21:12:49 +0300

    Iteration 1 is complete - all methods with separated decoration for primitives only

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,28 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    public static void log(int message) {
+
+    private static void decorateLogPrimitive(String message) {
         System.out.println("primitive: " + message);
     }
 
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    public static void log(int message) {
+        Facade.decorateLogPrimitive(String.valueOf(message));
+    }
+
+    public static void log(boolean message) {
+        Facade.decorateLogPrimitive(String.valueOf(message));
+    }
+
+    public static void log(char message) {
+        System.out.println("char: " + message);
+    }
+
+    public static void log(String message) {
+        System.out.println("string: " + message);
+    }
+
+    public static void log(Object message) {
+        System.out.println("reference: " + message);
     }
 }
