9d67794382a7743d873d6508b030203e9ac43a9e
Anton Liseitcev <antonlis2@yandex.ru>
2020-04-05 13:51:02 +0300

    iteration 4 - added enums

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,60 +2,56 @@ package com.acme.dbo.txlog;
 
 import java.util.Objects;
 
+import static com.acme.dbo.txlog.LoggerTypes.BYTE_TYPE;
+import static com.acme.dbo.txlog.LoggerTypes.INT_TYPE;
+import static com.acme.dbo.txlog.LoggerTypes.STRING_TYPE;
+
 public class Facade {
 
-    private static final String PRIMITIVE_FORMAT = "primitive";
-    private static final String STRING_FORMAT = "string";
-
-    private static final String stringType = "string";
-    private static final String intType = "int";
-    private static final String byteType = "byte";
-    private static final String unbufferedType = "unbuffered";
-
     public static boolean isDecorated = false;
-    private static String previousType;
+    private static LoggerTypes previousType;
+
     private static String stringState = "";
     private static int intState;
     private static byte byteState;
     private static int stringStateCount;
 
     public static void log(int message) {
-        write(PRIMITIVE_FORMAT, String.valueOf(message), intType);
+        write(String.valueOf(message), INT_TYPE);
     }
 
     public static void log(byte message) {
-        write(PRIMITIVE_FORMAT, String.valueOf(message), byteType);
+        write(String.valueOf(message), BYTE_TYPE);
     }
 
     public static void log(boolean message){
-        write(PRIMITIVE_FORMAT, String.valueOf(message), unbufferedType);
+        write(String.valueOf(message), LoggerTypes.BOOLEAN_TYPE);
     }
 
     public static void log(Object message){
-        checkBuffer(unbufferedType);
-        write("reference", String.valueOf(message), unbufferedType);
+        write(String.valueOf(message), LoggerTypes.OBJECT_TYPE);
     }
 
     public static void log(char message){
-        write("char", String.valueOf(message), unbufferedType);
+        write(String.valueOf(message), LoggerTypes.CHAR_TYPE);
     }
 
     public static void log(String message){
-        write(STRING_FORMAT, String.valueOf(message), stringType);
+        write(String.valueOf(message), STRING_TYPE);
     }
 
-    private static void write(String prefix, String payLoad, String newType) {
-        checkBuffer(newType);
+    private static void write(String payLoad, LoggerTypes operationType) {
+        checkBuffer(operationType);
 
-        if (newType.equals(unbufferedType)){
-            System.out.println(decorate(prefix, payLoad));
+        if (!operationType.isBuffered()){
+            System.out.println(decorate(payLoad, operationType));
         } else {
-            accumulate(payLoad, newType);
+            accumulate(payLoad, operationType);
         }
-        previousType = newType;
+        previousType = operationType;
     }
 
-    private static void checkBuffer(String newType) {
+    private static void checkBuffer(LoggerTypes newType) {
         if (Objects.isNull(previousType)){
             previousType = newType;
         } else if (!newType.equals(previousType)){
@@ -63,23 +59,23 @@ public class Facade {
         }
     }
 
-    private static void accumulate(String payLoad, String newType) {
-        switch (newType){
-            case intType:
+    private static void accumulate(String payLoad, LoggerTypes operationType) {
+        switch (operationType){
+            case INT_TYPE:
                 int intPayload = Integer.valueOf(payLoad);
                 if (intPayload > Integer.MAX_VALUE - intState){
                     flush();
                 }
                 intState += intPayload;
                 break;
-            case byteType:
+            case BYTE_TYPE:
                 int bytePayload = Byte.valueOf(payLoad);
                 if (bytePayload > Byte.MAX_VALUE - byteState){
                     flush();
                 }
                 byteState += bytePayload;
                 break;
-            case stringType:
+            case STRING_TYPE:
                 if (stringState.equals("")){
                     stringState = payLoad;
                 }
@@ -95,8 +91,8 @@ public class Facade {
 
     public static void flush() {
         switch (previousType){
-            case stringType:
-                stringState = decorate(STRING_FORMAT, stringState);
+            case STRING_TYPE:
+                stringState = decorate(stringState, STRING_TYPE);
                 if (stringStateCount > 0){
                     System.out.println(String.format("%s (x%d)", stringState, ++ stringStateCount));
                 } else {
@@ -105,27 +101,27 @@ public class Facade {
                 stringStateCount = 0;
                 stringState = "";
                 break;
-            case intType:
-                System.out.println(decorate(PRIMITIVE_FORMAT, intState));
+            case INT_TYPE:
+                System.out.println(decorate(intState, INT_TYPE));
                 intState = 0;
                 break;
-            case byteType:
-                System.out.println(decorate(PRIMITIVE_FORMAT, byteState));
+            case BYTE_TYPE:
+                System.out.println(decorate(byteState, BYTE_TYPE));
                 byteState = 0;
         }
     }
 
-    private static String decorate(String prefix, Object payLoad){
+    private static String decorate(Object payLoad, LoggerTypes type){
         String stringPayLoad = String.valueOf(payLoad);
         if (isDecorated){
-            return prefix + ": " + stringPayLoad;
+            return type.getDecoratePrefix() + ": " + stringPayLoad;
         } else {
             return stringPayLoad;
         }
     }
 
     public static void clean(){
-        previousType = "";
+        previousType = null;
     }
 
 }
