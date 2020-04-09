34d065e635bf968da4db83c2664700fe6306b517
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-04-09 12:05:26 +0300

    Add inheritance and overriding demo.

---
+++ src/main/java/demo/InheritanceDemo.java
@@ -0,0 +1,51 @@
+package demo;
+
+import java.io.IOError;
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Objects;
+
+public class InheritanceDemo {
+    public static void main(String[] args) { //LSP
+        EmptyMessageCheckLoggerWriter write = new ConsoleWriter();
+        try {
+            String result = write.write("HW!");
+        } catch (IOException e) {
+            //...
+        }
+
+        System.out.println(result.toUpperCase());
+        //1M sloc: result = String
+        //result <-> ????
+    }
+}
+
+interface LoggerWriter {
+    public abstract void write(String message);
+}
+
+class EmptyMessageCheckLoggerWriter { // implements LoggerWriter {
+    private String destination;
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+//    @Override
+    String write(String message) throws IOException {
+        if (message == null || message.equals("")) throw new IllegalArgumentException();
+    }
+}
+
+class ConsoleWriter extends EmptyMessageCheckLoggerWriter {
+//    @Override
+    public String write(String message) throws CustomIOException {
+        super.write(message);
+        this.setDestination("filename");
+        System.out.println(message + " to destination: " + this.getDestination());
+    }
+}
