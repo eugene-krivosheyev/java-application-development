9d635abec8387ed5cd65483269814bad9d3f72ce
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-04-14 12:59:27 +0300

    Inheritance and Template Method pattern

---
+++ src/main/java/ooaddemo/TMDemo.java
@@ -0,0 +1,49 @@
+package ooaddemo;
+
+
+public class TMDemo {
+    public static void main(String[] args) {
+
+    }
+}
+
+//TM
+
+/**
+ * Template Method
+ */
+abstract class MyLogger {
+    private LogFilter filter;
+//    IService service = Simple, HPDService
+
+    public void log(String message) {
+        //...
+        if(filter.filter(message)) {
+//            service.coreBusinesLogic(message);
+        }
+        //.....
+    }
+
+    protected abstract void coreBusinesLogic(String message);
+}
+
+class SimpleMyLogger extends MyLogger {
+    private LogWriter saver;
+
+    @Override
+    protected void coreBusinesLogic(String message) {
+        saver.write(message);
+    }
+}
+
+class HPDecoratedMyLogger extends MyLogger {
+    @Override
+    protected void coreBusinesLogic(String message) {
+        System.out.println();
+
+    }
+
+    private String decoreate(String message) {
+        return "Harry Potter and " + message;
+    }
+}
