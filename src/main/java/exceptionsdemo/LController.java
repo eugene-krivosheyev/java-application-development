cb0decf53a573bd9df0ca7b74ecfe2494aca322f
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-04-23 09:25:14 +0300

    Trash with Suppressed exception

--- src/main/java/exceptionsdemo/LController.java
+++ src/main/java/exceptionsdemo/LController.java
@@ -1,17 +1,32 @@
 package exceptionsdemo;
 
+import java.sql.SQLException;
+
 public class LController {
     private final ISaver saver = new LSaver();
 
     public void log(String message) throws LogException {
+        Connection conn = null;
+        LogException logException = null;
         try {
+            conn = ???;
             //..
             saver.save(message);
             //...
         } catch (SaveException | ArithmeticException e) {
-            throw new LogException("Can't save " + message, e);
+            logException = new LogException("Can't save " + message, e);
+            throw logException;
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    if (logException != null) {
+                        e.addSuppressed(logException);
+                    }
+                    throw e;
+                }
         }
     }
 }
