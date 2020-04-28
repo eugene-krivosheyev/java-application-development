5ae459253ad58d1d0de611e0efd1201b3a00d2d0
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-04-28 12:05:53 +0300

    Add Stream API demo

--- src/main/java/innernestedclassesdemo/InnerClassesDemo.java
+++ src/main/java/innernestedclassesdemo/InnerClassesDemo.java
@@ -1,6 +1,10 @@
 package innernestedclassesdemo;
 
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class InnerClassesDemo {
     private static int globalVar;
     private int objectState;
@@ -10,6 +14,7 @@ public class InnerClassesDemo {
         DemoWriter writer = String::toUpperCase; //new ???
         writer.write("");
 
+        new LlController(System.out::println);
         new DemoLogger( System.out::println );
         new DemoLogger(new DemoWriter() {
             @Override
@@ -17,6 +22,37 @@ public class InnerClassesDemo {
                 System.out.println(message);
             }
         });
+
+        int context = 5;
+        templateMethod(() -> Integer.toString(context));
+
+        System.out.println("========================");
+        List<Integer> cats = Arrays.asList(1,2,3,4,5);
+        cats.parallelStream() //Stream API || CompletableFuture
+                .map(e -> e + 1)
+                .filter(e -> e <= 3)
+            .forEach(System.out::println);
+
+
+    }
+
+    static ToDo templateMethod(ToDo todo) {
+        //.....
+        //.....
+        System.out.println( todo.dodo() );
+        //.....
+        //.....
+        return () -> "";
+    }
+}
+
+@FunctionalInterface
+interface ToDo {
+    String dodo();
+}
+
+class LlController {
+    public LlController(DemoWriter w) {
     }
 }
 
