11c13cfedf483ad7f3ba79cb9e0775dd685f7335
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2020-04-28 11:08:00 +0300

    Add lambdas

--- src/main/java/innernestedclassesdemo/InnerClassesDemo.java
+++ src/main/java/innernestedclassesdemo/InnerClassesDemo.java
@@ -7,16 +7,20 @@ public class InnerClassesDemo {
 
     public static void main(String[] args) {
         int localVar = 17;
+        DemoWriter writer = m -> m.toUpperCase(); //new ???
+        writer.write("");
 
+        new DemoLogger( message -> System.out.println(message));
         new DemoLogger(new DemoWriter() {
             @Override
             public void write(String message) {
-
+                System.out.println(message);
             }
         });
     }
 }
 
+@FunctionalInterface
 interface DemoWriter {
     void write(String message);
 }
