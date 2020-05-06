758fb21fb12c08c09aa34d96608099eea7ce3f0f
eugene.krivosheyev@gmail.com <eugene.krivosheyev@gmail.com>
2020-05-06 10:11:28 +0300

    Cloning demo.

---
+++ src/main/java/CloneDemo.java
@@ -0,0 +1,44 @@
+public class CloneDemo {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        Sheep clonedSheep =
+                new Sheep(1,"dolly",10).clone();
+
+        System.out.println(
+            clonedSheep.getId() +
+            clonedSheep.getMessage() +
+            clonedSheep.getWoolSize());
+
+    }
+}
+
+class Sheep implements Cloneable {
+    private int id;
+    private String message;
+    private int woolSize;
+    Sheep[] friends = new Sheep[10];
+
+    public Sheep(int id, String message, int woolSize) {
+        this.id = id;
+        this.message = message;
+        this.woolSize = woolSize;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getWoolSize() {
+        return woolSize;
+    }
+
+    @Override
+    public Sheep clone() throws CloneNotSupportedException {
+        //this.friends.clone()//
+        return (Sheep) super.clone();
+    }
+}
+
