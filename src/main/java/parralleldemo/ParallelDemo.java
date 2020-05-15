49452cb0af2bfb829a9bd03a396b461f61e5edc9
eugene.krivosheyev@gmail.com <eugene.krivosheyev@gmail.com>
2020-05-15 10:02:37 +0300

    Thread creation v03

--- src/main/java/parralleldemo/ParallelDemo.java
+++ src/main/java/parralleldemo/ParallelDemo.java
@@ -2,20 +2,18 @@ package parralleldemo;
 
 public class ParallelDemo {
     public static void main(String[] args) {
-        final Thread myWorker = new Thread(new MyWorker());
+        final Thread myWorker = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    System.out.println(Thread.currentThread().getName());
+                }
+            }
+        });
         //....
         myWorker.start();
         while (true) {
             System.out.println("main");
-        }
-    }
-}
-
-class MyWorker implements Runnable {
-    @Override
-    public void run() {
-        while (true) {
-            System.out.println(Thread.currentThread().getName());
         }
     }
 }
