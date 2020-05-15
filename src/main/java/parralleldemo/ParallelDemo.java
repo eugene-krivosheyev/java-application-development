0f84f976445ea57ba6441a3a0fcfb2d2d00b9c5a
eugene.krivosheyev@gmail.com <eugene.krivosheyev@gmail.com>
2020-05-15 10:44:18 +0300

    Add thread pools demo.

--- src/main/java/parralleldemo/ParallelDemo.java
+++ src/main/java/parralleldemo/ParallelDemo.java
@@ -1,16 +1,19 @@
 package parralleldemo;
 
+import java.util.concurrent.Executor;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 public class ParallelDemo {
-    public static void main(String[] args) {
-        final Thread myWorker = new Thread(() -> {
-            while (true) {
-                System.out.println(Thread.currentThread().getName());
-            }
-        });
-        //....
-        myWorker.start();
-        while (true) {
-            System.out.println("main");
-        }
+    public static void main(String[] args) throws InterruptedException {
+        ExecutorService threadPool = Executors.newFixedThreadPool(5);
+        threadPool.execute(() -> System.out.println(Thread.currentThread().getName()));
+        threadPool.execute(() -> System.out.println("HW!!!!!"));
+
+        threadPool.shutdown();
+        threadPool.awaitTermination(10, TimeUnit.SECONDS);
+
+        System.out.println("main");
     }
 }
