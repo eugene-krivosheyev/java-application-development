c37f114db2a81fb9af0d6a49b893968583a86e57
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-02-25 10:58:39 +0300

    Add Contract demo.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,28 @@
 package com.acme.dbo.txlog;
 
+/**
+ * Facade
+ * @author kjghdkfjghdkfjhg
+ * @since 1.0
+ */
 public class Facade {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+    /**
+     * message != null
+     */
+    //@Contract(!null -> !null)
+    public static void log(/* @NotNull */ int message) {
+        if (message <= 0) throw new IllegalArgumentException();
+        //--implementation как?--
+        printToConsole(message);
     }
 
     public static void log(byte message) {
+        printToConsole(message);
+    }
+
+
+    private static void printToConsole(int message) {
         System.out.println("primitive: " + message);
     }
 }
+// single-line
