42ec00e4849d17d46658c6523c9342550268a922
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-02-25 15:58:36 +0300

    Add contract demo.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,36 @@
 package com.acme.dbo.txlog;
 
+
 public class Facade {
-    public static void log(int message) {
-        System.out.println("primitive: " + message);
+    public static int counter = 0;
+
+
+    /** Contract:
+     * logs <b>ogogoggo!</b> message
+     * @param message must be positive
+     * @throws Exception
+     * @see
+     * @since 1.0
+     */
+//    @Contract(!null -> null, pure)
+    public static void log(/* @NonNull */ int message) throws IllegalArgumentException {
+        //assumeThatParamIsPosistive(message);
+
+        if (message <= 0) throw new IllegalArgumentException();
+        //~~implementation comment~~
+        final String decoratedMessage = "primitive: " + message;
+        printToConsole(decoratedMessage);
     }
 
+    /*
+    multi-line
+     */
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printToConsole("primitive: " + message);
+    }
+
+
+    private static void printToConsole(String decoratedMessage) {
+        System.out.println(decoratedMessage);
     }
 }
