5250f77ddf29685a907d0b140c242a5b66d2dd62
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-02-25 13:56:43 +0300

    Add namings demo.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,19 +1,28 @@
 package com.acme.dbo.txlog;
 
+import javax.security.auth.*;
+
 /**
  * Facade
  * @author kjghdkfjghdkfjhg
  * @since 1.0
  */
+
 public class Facade {
+    public static int переменнаяИзНескольктихСлов = 1; //static, global
+    public static final int MY_CONST = 2;
+
     /**
      * message != null
      */
     //@Contract(!null -> !null)
     public static void log(/* @NotNull */ int message) {
+        int i喝шш; //stack, auto, local
+
         if (message <= 0) throw new IllegalArgumentException();
         //--implementation как?--
         printToConsole(message);
+        //...
     }
 
     public static void log(byte message) {
