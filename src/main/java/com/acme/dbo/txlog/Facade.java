aa5cf490fd3fea1dca4a6d0effad545be18691b3
pod <a.struchkov@medpoint24.ru>
2021-03-04 15:10:59 +0300

    Test refactor to use multiple separators; Facade improve to avoid duplicates

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,16 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    public static void log(int message) {
+
+    public static void printMessage(String message) {
         System.out.println("primitive: " + message);
     }
 
+    public static void log(int message) {
+        printMessage(Integer.toString(message));
+    }
+
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printMessage(Byte.toString(message));
     }
 }
