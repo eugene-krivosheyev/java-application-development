094cfae1011a2a952cef64d7d96b7690d64ec63d
dmov <dimnrty@mail.ru>
2021-03-04 10:38:25 +0300

    fixes

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,17 +2,11 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     public static void log(int message) {
-        print(message);
+        prntPrimitive(message);
     }
 
     public static void log(byte message) {
-        print(message);
-    }
-
-    private static void print(Object message) {
-        if (message instanceof Integer || message instanceof Byte) {
-            prntPrimitive(message);
-        }
+        prntPrimitive(message);
     }
 
     private static void prntPrimitive(Object message) {
