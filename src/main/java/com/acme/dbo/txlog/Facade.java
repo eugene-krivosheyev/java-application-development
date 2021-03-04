301947e09c8884b5ba93531578f4c128c43a680a
dmov <dimnrty@mail.ru>
2021-03-04 10:22:24 +0300

    extracting prntPrimitive method

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,10 +2,26 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        print(message);
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        print(message);
+    }
+
+    private static void print(Object message) {
+        if (message instanceof Integer) {
+            prntPrimitive(message);
+        } else if (message instanceof Byte) {
+            prntPrimitive(message);
+        }
+    }
+
+    private static void prntPrimitive(Object message) {
+        prntToConsole("primitive: " + message);
+    }
+
+    private static void prntToConsole(Object message) {
+        System.out.println(message);
     }
 }
