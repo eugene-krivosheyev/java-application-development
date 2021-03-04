3b509c682a7f5b6b652c78398def5756ba3ff20c
rmatafonov <matafonov.rv@gmail.com>
2021-03-04 10:14:26 +0300

    Correct LoggerTest.java

--- src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration01/LoggerTest.java
@@ -6,12 +6,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-
 public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //region given
     @Before
-    public void setUpSystemOut() throws IOException {
+    public void setUpSystemOut() {
         resetOut();
         captureSysout();
     }
@@ -23,7 +21,7 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //endregion
 
     @Test
-    public void shouldLogInteger() throws IOException {
+    public void shouldLogInteger() {
         //region when
         Facade.log(1);
         Facade.log(0);
@@ -32,16 +30,23 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
 
         //region then
         assertSysoutContains("primitive: ");
-        assertSysoutEquals("primitive: 1\nprimitive: 0\nprimitive: -1\n");
+        assertSysoutContains("1");
+        assertSysoutContains("0");
+        assertSysoutContains("-1");
+        assertSysoutEquals(
+                "primitive: 1" + System.lineSeparator() +
+                        "primitive: 0" + System.lineSeparator() +
+                        "primitive: -1" + System.lineSeparator()
+        );
         //endregion
     }
 
     @Test
-    public void shouldLogByte() throws IOException {
+    public void shouldLogByte() {
         //region when
-        Facade.log((byte)1);
-        Facade.log((byte)0);
-        Facade.log((byte)-1);
+        Facade.log((byte) 1);
+        Facade.log((byte) 0);
+        Facade.log((byte) -1);
         //endregion
 
         //region then
@@ -49,6 +54,11 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         assertSysoutContains("1");
         assertSysoutContains("0");
         assertSysoutContains("-1");
+        assertSysoutEquals(
+                "primitive: 1" + System.lineSeparator() +
+                        "primitive: 0" + System.lineSeparator() +
+                        "primitive: -1" + System.lineSeparator()
+        );
         //endregion
     }
 
