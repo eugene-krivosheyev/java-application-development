b4dd9768778b4e9472a8a0456cb1938db5b086a7
Alexey Tarasovskiy <alexey.tarasovskiy@gmail.com>
2021-03-04 15:10:50 +0300

    iteration 01

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,19 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    String separator = "primitive: ";
+
+    public static void writeToConsole(String separator,String message)
+    {
+        System.out.println(separator + message);
+    }
+
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+       String separator = "primitive: ";
+        writeToConsole (separator, Integer.toString(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        System.out.println("primitive: " + Byte.toString(message));
     }
 }
