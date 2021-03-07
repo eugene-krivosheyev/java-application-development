8c43b66e295c45889ab3d91914d24414b24c7b7f
YuliyaUt <ut.yuliya@yandex.ru>
2021-03-07 23:31:31 +0300

    Iteration 00

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,10 +2,18 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        printToConsole(decorate(message));
     }
 
     public static void log(byte message) {
-        System.out.println("primitive: " + message);
+        printToConsole(decorate(message));
+    }
+
+    private static String decorate(Object message) {
+        return "primitive: " + message;
+    }
+
+    private static void printToConsole(Object message) {
+        System.out.println(message);
     }
 }
