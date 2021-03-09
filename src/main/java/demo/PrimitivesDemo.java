198584484aa608f638ae75b76751e060191deabf
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-09 14:55:15 +0300

    Add primitives demo: Part 3, chars.

--- src/main/java/demo/PrimitivesDemo.java
+++ src/main/java/demo/PrimitivesDemo.java
@@ -1,18 +1,26 @@
 package demo;
 
-public class PrimitivesDemo {
-    public static void main(String[] args) {
+public strictfp class PrimitivesDemo {
+    public strictfp static void main(String[] args) {
         //region целочисленные
         byte b = 0b10; // 1 bytes; --0..255-- ^ -128..127
         short s = 010; // 2 bytes;
         int i = 0xABCDEF; // 4 bytes;
         long l = 99_999_999_999L; // 8 bytes;
-        l = 3;
-        l++;
-        System.out.println("dfsdfgdf" + Long.MAX_VALUE);
+        System.out.println(">> " + Long.MAX_VALUE);
         //endregion
 
         //region FP
+        //IEEE 754
+        float f = -0.f; // 32 bit
+        double d = -3e-15; // 64 bit
+        //endregion
+
+        //region символьный
+        //l10n and i18n
+        char c = '\uABCE'; // 2 byte, ꯎ
+        int charCode = c;
+        System.out.println('\t'); //CSV, TSV
         //endregion
 
         //region Wrappers
