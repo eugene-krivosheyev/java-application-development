308ee0055dcbd615d90d0d19f460576b30701654
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-09 13:57:21 +0300

    Add primitives demo: Part 1.

---
+++ src/main/java/demo/PrimitivesDemo.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class PrimitivesDemo {
+    public static void main(String[] args) {
+        //region целочисленные
+        byte b = 0b10; // 1 bytes; --0..255-- ^ -128..127
+        short s = 010; // 2 bytes;
+        int i = 0xABCDEF; // 4 bytes;
+        long l = 99_999_999_999L; // 8 bytes;
+        l = 3;
+        l++;
+        System.out.println("dfsdfgdf" + Long.MAX_VALUE);
+        //endregion
+
+        //region Wrappers
+
+        //endregion
+    }
+}
