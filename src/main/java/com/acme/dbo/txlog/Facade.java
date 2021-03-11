089601e0fed28cd2ee7eca82e4081320b0630332
pod <a.struchkov@medpoint24.ru>
2021-03-11 14:23:06 +0300

    Add param for primitive prefix; refactor printMessage

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,16 +2,19 @@ package com.acme.dbo.txlog;
 
 public class Facade {
 
+    private final static String PRIMITIVE_PREFIX = "primitive: ";
+
     public static void printMessage(String prefix, String message) {
-        System.out.println(prefix + message);
+        if (prefix.equals(PRIMITIVE_PREFIX)) { System.out.println(prefix + message);}
+        else {System.out.println(prefix + message);}
     }
 
     public static void log(int message) {
-        printMessage("primitive: ", Integer.toString(message));
+        printMessage(PRIMITIVE_PREFIX, Integer.toString(message));
     }
 
     public static void log(byte message) {
-        printMessage("primitive: ", Byte.toString(message));
+        printMessage(PRIMITIVE_PREFIX, Byte.toString(message));
     }
 
     public static void log(char message) {
@@ -23,7 +26,7 @@ public class Facade {
     }
 
     public static void log(boolean message) {
-        printMessage("primitive: ", Boolean.toString(message));
+        printMessage(PRIMITIVE_PREFIX, Boolean.toString(message));
     }
 
     public static void log(Object message) { printMessage("reference: ", String.valueOf(message)); }
