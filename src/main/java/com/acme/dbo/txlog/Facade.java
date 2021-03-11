ac630fe35a967591ccd8d6ed9bf2b74ee0d75503
pod <a.struchkov@medpoint24.ru>
2021-03-11 15:15:08 +0300

    Add decorate method; refactor Facade

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -3,31 +3,39 @@ package com.acme.dbo.txlog;
 public class Facade {
 
     private final static String PRIMITIVE_PREFIX = "primitive: ";
-
-    public static void printMessage(String prefix, String message) {
-        if (prefix.equals(PRIMITIVE_PREFIX)) { System.out.println(prefix + message);}
-        else {System.out.println(prefix + message);}
-    }
+    private final static String STRING_PREFIX = "string: ";
+    private final static String CHAR_PREFIX = "char: ";
+    private final static String REFERENCE_PREFIX = "reference: ";
 
     public static void log(int message) {
-        printMessage(PRIMITIVE_PREFIX, Integer.toString(message));
+        printMessage(decorate(PRIMITIVE_PREFIX, message));
     }
 
     public static void log(byte message) {
-        printMessage(PRIMITIVE_PREFIX, Byte.toString(message));
+        printMessage(decorate(PRIMITIVE_PREFIX, message));
     }
 
     public static void log(char message) {
-        printMessage("char: ", Character.toString(message));
+        printMessage(decorate(CHAR_PREFIX, message));
     }
 
     public static void log(String message) {
-        printMessage("string: ", message);
+        printMessage(decorate(STRING_PREFIX, message));
     }
 
     public static void log(boolean message) {
-        printMessage(PRIMITIVE_PREFIX, Boolean.toString(message));
+        printMessage(decorate(PRIMITIVE_PREFIX, message));
     }
 
-    public static void log(Object message) { printMessage("reference: ", String.valueOf(message)); }
+    public static void log(Object message) {
+        printMessage(decorate(REFERENCE_PREFIX, message));
+    }
+
+    private static void printMessage(Object message) {
+        System.out.println(message);
+    }
+
+    private static String decorate(String prefix, Object message) {
+        return prefix + message;
+    }
 }
