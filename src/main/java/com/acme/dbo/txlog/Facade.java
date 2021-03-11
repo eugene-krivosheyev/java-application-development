ce801e72e75b9feab0159c52d4cab3e853ddcf45
dropnz <dropnz.fw@gmail.com>
2021-03-11 13:55:37 +0300

    iteration01: implement Logger solution to match specification as tests

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,13 +2,31 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     private static final String primitivePrefix = "primitive: ";
+    private static final String primitiveCharPrefix = "char: ";
+    private static final String primitiveStringPrefix = "string: ";
+    private static final String primitiveReferencePrefix = "reference: ";
 
+    /**
+       Print to console int and byte.
+     */
     public static void log(int message) {
         printToConsole(primitivePrefix + message);
     }
 
-    public static void log(byte message) {
-        printToConsole(primitivePrefix + message);
+    public static void log(boolean b) {
+        printToConsole(primitivePrefix + b);
+    }
+
+    public static void log(char c) {
+        printToConsole(primitiveCharPrefix + c);
+    }
+
+    public static void log(String s) {
+        printToConsole(primitiveStringPrefix + s);
+    }
+
+    public static void log(Object obj) {
+        printToConsole(primitiveReferencePrefix + obj);
     }
 
     private static void printToConsole(String message) {
