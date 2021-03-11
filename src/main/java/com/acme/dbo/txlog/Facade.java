576a08c6616aef05cd876e160a5d6ed855bb63fb
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-11 15:42:29 +0300

    Add decorating logger demo.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,15 +1,33 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String STRING_PREFIX = "string prefix: ";
+    private static final String PRIMITIVE_POSTFIX = "";
+    private static final String STRING_POSTFIX = "";
+
     public static void log(int message) {
-        print("primitive: " + message);
+        print(decorate(PRIMITIVE_PREFIX, message, PRIMITIVE_POSTFIX));
     }
 
     public static void log(byte message) {
-        print("primitive: " + message);
+        print(decorate(PRIMITIVE_PREFIX, message, PRIMITIVE_POSTFIX));
     }
 
-    private static void print(String message) {
+    public static void log(String message) {
+        print(decorate(STRING_PREFIX, message, STRING_POSTFIX));
+    }
+
+    public static void log(Object message) {
+        print(decorate("", message, ""));
+    }
+
+
+    private static void print(Object message) {
         System.out.println(message);
+    }
+
+    private static String decorate(String prefix, Object message, String postfix) {
+        return prefix + message + postfix;
     }
 }
