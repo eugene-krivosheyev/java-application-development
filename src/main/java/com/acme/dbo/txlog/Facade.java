6e02fe30c4c5da6301be91442c615f46c693aa25
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2021-03-11 11:16:34 +0300

    iteration 01 - added decorator

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,28 +1,20 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    private static final String PRIMITIVE_PREFIX = "primitive: ";
-    private static final String CHAR_PREFIX = "char: ";
-    private static final String STRING_PREFIX = "string: ";
-    private static final String OBJECT_PREFIX = "reference: ";
 
     public static void log(Object obj) {
         printMessage(decorate(obj));
     }
 
     private static String decorate(Object message) {
-        String prefix = OBJECT_PREFIX;
+        String prefix = "reference: "; // default one, if need custom case - modify below
 
-        if (message instanceof Integer) {
-            prefix = PRIMITIVE_PREFIX;
-        } else if (message instanceof Byte) {
-            prefix = PRIMITIVE_PREFIX;
+        if ((message instanceof Integer) || (message instanceof Byte) || (message instanceof Boolean)) {
+            prefix = "primitive: ";
         } else if (message instanceof String ) {
-            prefix = STRING_PREFIX;
-        } else if (message instanceof Boolean) {
-            prefix = PRIMITIVE_PREFIX;
+            prefix = "string: ";
         } else if (message instanceof Character) {
-            prefix = CHAR_PREFIX;
+            prefix = "char: ";
         }
 
         return prefix + message;
