b0b81bbe62cd5f8e8d5490532bda0231b9dc5fd5
alex.samarkin <alexgui@yandex.ru>
2021-03-11 14:04:40 +0300

    Iteration 1

--- src/main/java/com/acme/dbo/txlog/OutputDecorator.java
+++ src/main/java/com/acme/dbo/txlog/OutputDecorator.java
@@ -1,6 +1,11 @@
 package com.acme.dbo.txlog;
 
-public class OutputDecorator implements Logger{
+public class OutputDecorator implements Logger {
+
+    private static String PRIMITIVE_PREFIX = "primitive: ";
+    private static String CHAR_PREFIX = "char: ";
+    private static String STRING_PREFIX = "string: ";
+    private static String REFERENCE_PREFIX = "reference: ";
 
     private Logger logger;
 
@@ -9,30 +14,30 @@ public class OutputDecorator implements Logger{
     }
 
     public void log(byte message) {
-        logger.log("primitive: " + message);
+        logger.log(PRIMITIVE_PREFIX + message);
     }
 
     public void log(String message) {
-        logger.log("string: " + message);
+        logger.log(STRING_PREFIX + message);
     }
 
     public void log(char message) {
-        logger.log("char: " + message);
+        logger.log(CHAR_PREFIX + message);
     }
 
     public void log(int message) {
-        logger.log("primitive: " + message);
+        logger.log(PRIMITIVE_PREFIX + message);
     }
 
     public void log(float message) {
-        logger.log("primitive: " + message);
+        logger.log(PRIMITIVE_PREFIX + message);
     }
 
     public void log(boolean message) {
-        logger.log("primitive: " + message);
+        logger.log(PRIMITIVE_PREFIX + message);
     }
 
     public void log(Object message) {
-        logger.log("reference: " + message);
+        logger.log(REFERENCE_PREFIX + message);
     }
 }
