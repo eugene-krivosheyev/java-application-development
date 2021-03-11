cbebe0de73764375c29135b9d124fb41dccabcad
alex <zelenskya@gmail.com>
2021-03-11 10:42:40 +0300

    Removes duplicated code.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -4,59 +4,39 @@ package com.acme.dbo.txlog;
 
 public class Facade {
 
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+
     //region log methods
     public static void log(int message) {
         if (message >= 0)
-            { printout(message); }
+            { printMessage(PRIMITIVE_PREFIX + message); }
         else
             throw new IllegalArgumentException("Message is less than 0.");
     }
 
     public static void log(byte message) {
-        printout(message);
+        printMessage(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(char message) {
-        printout(message);
+        printMessage("char: " + message);
     }
 
     public static void log(String message) {
-        printout(message);
+        printMessage("string: " + message);
     }
 
     public static void log(boolean message) {
-        printout(message);
+        printMessage(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(Object message) {
-        printout(message);
+        printMessage("reference: " + message);
     }
     //endregion
 
-    //region printout methods
-    private static void printout(int message) {
+    private static void printMessage(Object message) {
 
-        System.out.println("primitive: " + message);
+        System.out.println(message);
     }
-
-    private static void printout(char message) {
-
-        System.out.println("char: " + message);
-    }
-
-    private static void printout(String message) {
-
-        System.out.println("string: " + message);
-    }
-
-    private static void printout(boolean message) {
-
-        System.out.println("primitive: " + message);
-    }
-
-    private static void printout(Object message) {
-
-        System.out.println("reference: " + message);
-    }
-    //endregion
 }
