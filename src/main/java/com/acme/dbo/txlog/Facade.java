b26ca7485adb95c840d1bebf73cbebd18bcf29b0
Yulia <youlia.tarasova@gmail.com>
2021-03-11 10:49:58 +0300

    refactoring: add string literals for message types

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,28 +1,33 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    public static final String PRIMITIVE_PREFIX = "primitive: ";
+    public static final String STRING_PREFIX = "string: ";
+    public static final String CHAR_PREFIX = "char: ";
+    public static final String REFERENCE_PREFIX = "reference: ";
+
     public static void log(int message) {
-        printMessage("primitive: " + message);
+        printMessage(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(char message) {
-        printMessage("char: " + message);
+        printMessage(CHAR_PREFIX + message);
     }
 
     public static void log(String message) {
-        printMessage("string: " + message);
+        printMessage(STRING_PREFIX + message);
     }
 
     public static void log(byte message) {
-        printMessage("primitive: " + message);
+        printMessage(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(boolean message) {
-        printMessage("primitive: " + message);
+        printMessage(PRIMITIVE_PREFIX + message);
     }
 
     public static void log(Object message) {
-        printMessage("reference: " + message);
+        printMessage(REFERENCE_PREFIX + message);
     }
 
     private static void printMessage(String message) {
