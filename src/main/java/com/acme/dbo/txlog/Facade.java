1b0cf7ff8c65bed0ee67dc1c7510ca54fc07d42c
YuliyaUt <ut.yuliya@yandex.ru>
2021-03-11 10:35:47 +0300

    Iteration 01 - refactoring

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -2,27 +2,27 @@ package com.acme.dbo.txlog;
 
 public class Facade {
     public static void log(int message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     public static void log(byte message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     public static void log(char message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     public static void log(String message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     public static void log(boolean message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     public static void log(Object message) {
-        printToConsole(decorate(message));
+        printMessage(decorate(message));
     }
 
     private static String decorate(int message) {
@@ -49,7 +49,7 @@ public class Facade {
         return "reference: " + message;
     }
 
-    private static void printToConsole(String message) {
+    private static void printMessage(String message) {
         System.out.println(message);
     }
 }
