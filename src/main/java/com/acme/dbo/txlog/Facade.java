539155faef895987c7e851ee69995f842c238328
Alex V. Solo <lexxan@bk.ru>
2021-03-11 10:18:04 +0300

    Tests fixe

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -21,28 +21,35 @@ public class Facade {
      */
     //@Contract(!null -> !null)
     public static void log(/* @NotNull */ int message) { //formal arg
-        printToConsole(message);
+        printToConsole("primitive: " + message + '\n');
+    }
+
+    public static void log(char message) {
+        printToConsole("char: " + message);
     }
 
     public static void log(byte message) {
-        printToConsole(message);
+        printToConsole("primitive: " + message);
     }
 
-    public static String log(String message) {
-        return "";
+    public static void log(String message) {
+        printToConsole("string: " + message);
+    }
+
+    public static void log(boolean message) {
+        printToConsole("primitive: " + message + '\n');
     }
 
     public static void log(Object message) {
-        return;
+        printToConsole("reference: @" + message + '\n');;
     }
 
     public static void log(String message1, String message2) {
 
     }
 
-
-    private static void printToConsole(int message) {
-        System.out.print("primitive: " + message + "\n");
+    private static void printToConsole(String message) {
+        System.out.print(message);
     }
 }
 // single-line
