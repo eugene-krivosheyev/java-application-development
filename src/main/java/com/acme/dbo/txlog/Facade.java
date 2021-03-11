2f73cdd3e3b9f0fc3ecae37703c9d9f8a688604e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-11 14:17:51 +0300

    Cleanup.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,35 +1,15 @@
 package com.acme.dbo.txlog;
 
-import java.sql.DriverManager;
-import java.util.function.Function;
-
-/**
- *
- */
 public class Facade {
-    public static final String PRIMITIVE_PREFIX = "primitive: ";
-
-    public static int counter = 0; //global state -> behavior
-
-    /** Contract:
-     * logs <b>ogogoggo!</b> message
-     * @param message must be positive
-     * @throws Exception
-     * @see
-     * @since 1.0
-     */
-    public static void log(int message) throws IllegalArgumentException {
-        print(message);
+    public static void log(int message) {
+        print("primitive: " + message);
     }
 
-    /*
-    multi-line
-     */
-    public static void log(byte message) { //formal par
-        print(message);
+    public static void log(byte message) {
+        print("primitive: " + message);
     }
 
-    private static void print(int message) {
-        System.out.println(PRIMITIVE_PREFIX + message);
+    private static void print(String message) {
+        System.out.println(message);
     }
 }
