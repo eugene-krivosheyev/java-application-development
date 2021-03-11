42c11530817398f1a02a4e66e2dfd589309a3bd8
PrikhodkoStanislav <prikhodko-stanislav@yandex.ru>
2021-03-11 11:11:35 +0300

    iteration 01 - added decorator

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,26 +1,32 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
-    public static void log(int message) {
-        printMessage("primitive: " + message);
-    }
-
-    public static void log(char message) {
-        printMessage("char: " + message);
-    }
-
-    public static void log(String message) {
-        printMessage("string: " + message);
-    }
-
-    public static void log(boolean message) {
-        printMessage("primitive: " + message);
-    }
+    private static final String PRIMITIVE_PREFIX = "primitive: ";
+    private static final String CHAR_PREFIX = "char: ";
+    private static final String STRING_PREFIX = "string: ";
+    private static final String OBJECT_PREFIX = "reference: ";
 
     public static void log(Object obj) {
-        printMessage("reference: " + obj);
+        printMessage(decorate(obj));
     }
 
+    private static String decorate(Object message) {
+        String prefix = OBJECT_PREFIX;
+
+        if (message instanceof Integer) {
+            prefix = PRIMITIVE_PREFIX;
+        } else if (message instanceof Byte) {
+            prefix = PRIMITIVE_PREFIX;
+        } else if (message instanceof String ) {
+            prefix = STRING_PREFIX;
+        } else if (message instanceof Boolean) {
+            prefix = PRIMITIVE_PREFIX;
+        } else if (message instanceof Character) {
+            prefix = CHAR_PREFIX;
+        }
+
+        return prefix + message;
+    }
 
     private static void printMessage(String msg) {
         System.out.println(msg);
