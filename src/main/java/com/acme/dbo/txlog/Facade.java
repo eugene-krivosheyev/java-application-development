62bfc2e73c190aa9d5c724c49cf45ab79b5b72ff
danilishe <suetin.danil@gmail.com>
2021-03-11 10:31:38 +0400

    iter 2 st 1

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,16 +1,29 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+    private static boolean isLoggedIntOnPrevious = false;
+    private static int summ = 0;
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        isLoggedIntOnPrevious = true;
+        summ += message;
+        if (summ == 0) printSumm();
     }
 
     public static void log(byte message) {
         System.out.println("primitive: " + message);
     }
 
+    private static void printSumm() {
+        if (isLoggedIntOnPrevious) {
+            System.out.println(summ);
+            summ = 0;
+            isLoggedIntOnPrevious = false;
+        }
+    }
+
     public static void log(String message) {
-        System.out.println("string: " + message);
+        printSumm();
+        System.out.println(message);
     }
 
     public static void log(char message) {
--- src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration02/LoggerTest.java
@@ -1,8 +1,10 @@
 package com.acme.dbo.txlog.iteration02;
 
+import com.acme.dbo.txlog.Facade;
 import com.acme.dbo.txlog.SysoutCaptureAndAssertionAbility;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
 
@@ -20,9 +22,6 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     }
     //endregion
 
-
-    /*
-    TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogSequentIntegersAsSum() throws IOException {
@@ -43,6 +42,8 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
         );
         //endregion
     }
+
+    /*
 
     @Test
     public void shouldLogCorrectlyIntegerOverflowWhenSequentIntegers() {
