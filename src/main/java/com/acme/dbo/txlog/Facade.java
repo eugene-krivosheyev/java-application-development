7c9608874429cf7dd998fb4da63679f9e2b2c7b0
alex-dylda <sd1990@yandex.ru>
2021-03-11 09:56:43 +0300

    uncomment tests refactor facade to tests passed

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -3,15 +3,36 @@ package com.acme.dbo.txlog;
 public class Facade {
     public static void log(int message) {
 
-        printIntToConsole(message);
+        printToConsole("primitive: " + message);
     }
 
     public static void log(byte message) {
 
-        printIntToConsole(message);
+        printToConsole("primitive: " + message);
     }
 
-    private static void printIntToConsole(int message) {
-        System.out.println("primitive: " + message);
+    public static void log(char message) {
+
+        printToConsole("char: " + message);
+    }
+
+    public static void log(String message) {
+
+        printToConsole("string: " + message);
+    }
+
+    public static void log(boolean message) {
+
+        printToConsole("primitive: " + message);
+    }
+
+    public static void log(Object message) {
+
+        printToConsole("reference: " + message);
+    }
+
+    private static void printToConsole(String message) {
+
+        System.out.println(message);
     }
 }
