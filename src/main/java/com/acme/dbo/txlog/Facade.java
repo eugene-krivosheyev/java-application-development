a08557ad42c0f22a0533f14b583deedb738605d5
Michael Shabynin <vectro73@gmail.com>
2021-03-11 14:11:05 +0300

    add prefix detector for every type in tests, not sure if it is the best design, but chose this over overloading.

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,7 +1,24 @@
 package com.acme.dbo.txlog;
 
 public class Facade {
+
     public static void log(Object message) {
-        System.out.println("primitive: " + message);
+        outputMessageWithPrefix(getPrefixByType(message), message);
+    }
+
+    private static void outputMessageWithPrefix(String prefix, Object message) {
+        System.out.println(prefix + ": " + message);
+    }
+
+    private static String getPrefixByType(Object message) {
+        if ((message instanceof Integer) | (message instanceof Byte) | (message instanceof Boolean)) {
+            return "primitive";
+        } else if (message instanceof String){
+            return "string";
+        } else if (message instanceof Character) {
+            return "char";
+        } else {
+            return "reference";
+        }
     }
 }
