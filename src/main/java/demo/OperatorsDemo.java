8423158db3470480da947d4d187c8f1abad8baee
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-16 14:46:39 +0300

    Add arithmetics promotion and arithmetics corner cases demo.

--- src/main/java/demo/OperatorsDemo.java
+++ src/main/java/demo/OperatorsDemo.java
@@ -5,6 +5,8 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static java.lang.Double.NaN;
+import static java.lang.Double.POSITIVE_INFINITY;
 import static java.util.Collections.emptyList;
 import static java.util.Optional.of;
 
@@ -18,7 +20,7 @@ public class OperatorsDemo {
         b = (byte) i; //unary operator
         System.out.println(b);
 
-        Object object = new Integer("abc"); // polymorphism
+        Object object = new Integer("1"); // polymorphism
         if (object instanceof String) {
             System.out.println(( (String) object )); // ClassCastException
         }
@@ -30,5 +32,23 @@ public class OperatorsDemo {
         int[] array = {1,2,3}; //
         Optional maybe = of("125.7.8.6:666"); // Factory Method
         final List<Object> objects = emptyList();
+
+        //region Arithmetics
+        // + - * / %
+        System.out.println( 1/3 );
+
+        byte b1 = 1, b2 = 2;
+        int i1 = b1 + b2;
+        long l = i1 + 1L;
+        double v = l + 1.;
+
+        System.out.println( 1 + 2 + "3" + 4 + 5 );
+
+        System.out.println(0./0.);
+        System.out.println(POSITIVE_INFINITY == POSITIVE_INFINITY);
+        System.out.println(POSITIVE_INFINITY + 1 == POSITIVE_INFINITY);
+        System.out.println(NaN == NaN);
+        System.out.println(NaN + 1 < NaN);
+        //endregion
     }
 }
