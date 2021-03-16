2c0c747a5667ae018332ea92e80b1260d66d5e7f
Yulia <youlia.tarasova@gmail.com>
2021-03-16 09:15:26 +0300

    refactoring: add decorator method

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -7,30 +7,30 @@ public class Facade {
     public static final String REFERENCE_PREFIX = "reference: ";
 
     public static void log(int message) {
-        printMessage(PRIMITIVE_PREFIX + message);
+        printMessage(decorate(PRIMITIVE_PREFIX,message));
     }
 
     public static void log(char message) {
-        printMessage(CHAR_PREFIX + message);
+        printMessage(decorate(CHAR_PREFIX, message));
     }
 
     public static void log(String message) {
-        printMessage(STRING_PREFIX + message);
+        printMessage(decorate(STRING_PREFIX, message));
     }
 
-    public static void log(byte message) {
-        printMessage(PRIMITIVE_PREFIX + message);
-    }
+    public static void log(byte message) { printMessage(decorate(PRIMITIVE_PREFIX, message)); }
 
     public static void log(boolean message) {
-        printMessage(PRIMITIVE_PREFIX + message);
+        printMessage(decorate(PRIMITIVE_PREFIX, message));
     }
 
-    public static void log(Object message) {
-        printMessage(REFERENCE_PREFIX + message);
-    }
+    public static void log(Object message) { printMessage(decorate(REFERENCE_PREFIX, message)); }
 
     private static void printMessage(String message) {
         System.out.println(message);
+    }
+
+    private static String decorate(String stringPrefix, Object message) {
+        return stringPrefix + message;
     }
 }
