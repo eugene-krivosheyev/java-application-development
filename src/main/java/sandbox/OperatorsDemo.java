62b2136779ad0e826ba1059c31bcf102f7af7d3a
JustMe <mpostbox@inbox.ru>
2021-03-16 13:56:38 +0300

    Logical ops, bitwise ops, float/double problems

--- src/main/java/sandbox/OperatorsDemo.java
+++ src/main/java/sandbox/OperatorsDemo.java
@@ -10,9 +10,31 @@ public class OperatorsDemo {
         //endregion
 
         //region NON primitives
-        if("asdasdad" instanceof String) {
-
+        Object obj = new String("a");
+        if(obj instanceof String) {
+            System.out.println((String)obj);
         }
+
+        //endregion
+
+        //region arithmetics ops
+        byte b1 = 1,b2 = 3;
+        double v = b1 / (double) b2;
+        System.out.println(Double.POSITIVE_INFINITY);
+        System.out.println(v);
+        System.out.println("");
+        //endregion
+
+        //region bitwise ops
+        int v1 = 2, v2 = 2, v3 = -1;
+        System.out.println(v1);
+        System.out.println(v1 & v2);
+        System.out.println(v1 | v2);
+        System.out.println(v1 ^ v2);
+        System.out.println(v1 << 1);
+        System.out.println(v2 >> 1);
+        System.out.println(v3 >>> 1); //signed shift
+        System.out.println(v3 << 1);
 
         //endregion
     }
