9fab2df1393363b78dee3eadf47e6aed6c230ac9
AlexNekh12 <mpostbox@inbox.ru>
2021-03-18 11:18:27 +0300

    refact guard clauses demo

--- src/main/java/sandbox/SwitchAndCyclesDemo.java
+++ src/main/java/sandbox/SwitchAndCyclesDemo.java
@@ -11,15 +11,17 @@ public class SwitchAndCyclesDemo {
         }
 
         int age = 30;
-        switch (age){
+        switch (age) {
             case 20: {
                 System.out.println("too yang!");
                 break;
             }
             case 30:
-                System.out.println("good enough"); break;
+                System.out.println("good enough");
+                break;
             case 40:
-                System.out.println("too yang!"); break;
+                System.out.println("too yang!");
+                break;
             default:
                 System.out.println("default");
 
@@ -32,5 +34,19 @@ public class SwitchAndCyclesDemo {
 
     private static boolean fun1() {
         return false;
+    }
+
+    private static void methodWithParam(int param) {
+        if (param > 0) {
+            //logic return false;
+        } else {
+            throw new IllegalArgumentException("param must be positive! Having: " + param);
+        }
+    }
+
+    // Guard clauses
+    private static void refactMethodWithParam(int param) {
+        if (param <= 0) throw new IllegalArgumentException("param must be positive! Having: " + param);
+        // body
     }
 }
