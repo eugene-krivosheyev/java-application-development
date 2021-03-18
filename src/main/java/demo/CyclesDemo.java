4ffeaedac94203c119d5d69f18b0b7e9d17a7ff9
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-18 15:56:59 +0300

    Add labels demo.

--- src/main/java/demo/CyclesDemo.java
+++ src/main/java/demo/CyclesDemo.java
@@ -4,6 +4,7 @@ import java.util.Objects;
 
 public class CyclesDemo {
     public static void main(String[] args) {
+        //region IF
         if (condition()) {
             System.out.println("111");
             System.out.println("111");
@@ -12,7 +13,9 @@ public class CyclesDemo {
         } else {
 
         }
+        //endregion
 
+        //region Switch
         String var = null; // == .equals()
         switch (var) {
             case "1": {
@@ -30,11 +33,46 @@ public class CyclesDemo {
         if (Objects.equals("1", var)) {
 
         }
+        //endregion
 
+        //region Loops
+        for(int counter = 0, j = 0; counter < 10; counter++, j++) {
+            //....
+        }
 
+        do {
 
+        } while (forever());
 
+        while(forever()) {
 
+        }
+
+        //foreach
+        for (String current : args) {
+            System.out.println(current);
+        }
+
+        outer: while (forever()) {
+            //...
+            inner: do {
+                if (condition()) break outer;
+                if (condition()) continue outer;
+                //...
+                third: for (int i = 0; i < 10; i++) {
+                    if (condition()) break; // -> 65
+                    if (condition()) continue; // -> 60
+                    //...
+                }
+                //...
+            } while (forever());
+            //...
+        }
+        //endregion
+    }
+
+    private static boolean forever() {
+        return true;
     }
 
     private static boolean condition2() {
