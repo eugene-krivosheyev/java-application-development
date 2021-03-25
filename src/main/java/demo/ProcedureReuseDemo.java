0c5231eaf72b8205cae3dfdb739a1ba79ead1901
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-25 09:30:26 +0300

    Add simple generalization demo.

---
+++ src/main/java/demo/ProcedureReuseDemo.java
@@ -0,0 +1,54 @@
+package demo;
+
+public class ProcedureReuseDemo {
+    public static void main(String[] args) {
+        //region before
+        if (condition()) {
+            m1(0);
+        } else {
+            m2(0);
+        }
+        //endregion
+
+        //region after
+        if (condition()) {
+            m(0, Byte.MAX_VALUE);
+        } else {
+            m(0, Integer.MAX_VALUE);
+        }
+        //endregion
+    }
+
+    //region before
+    private static void m1(int param) {
+        if (param >= Byte.MAX_VALUE) {
+            //1
+            //2
+            //3
+        }
+    }
+
+    private static void m2(int param) {
+        if (param >= Integer.MAX_VALUE) {
+            //1
+            //2
+            //3
+        }
+    }
+    //endregion
+
+    //region after
+    private static void m(int param, int limit) {
+        if (param >= limit) {
+            //1
+            //2
+            //3
+        }
+    }
+    //endregion
+
+
+    private static boolean condition() {
+        return false;
+    }
+}
