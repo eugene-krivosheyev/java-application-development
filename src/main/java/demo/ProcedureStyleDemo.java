8a34761d499d751d441e647b5c01b4f052ebbdd4
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-03-25 13:57:11 +0300

    Add behavior generalization demo.

--- src/main/java/demo/ProcedureStyleDemo.java
+++ src/main/java/demo/ProcedureStyleDemo.java
@@ -4,18 +4,21 @@ package demo;
 public class ProcedureStyleDemo {
     public static void main(String[] args) {
         if (condition()) {
-            m(0, Byte.MAX_VALUE);
+            m(0, Byte.MAX_VALUE, "option1");
         } else {
-            m(0, Integer.MAX_VALUE);
+            m(0, Integer.MAX_VALUE, "option2");
         }
     }
 
     //region after
-    private static void m(int param, int limit) {
+    private static void m(int param, int limit, String option) {
         if (param >= limit) {
-            //
+            //op
         } else {
-            //
+            switch (option) { //OCP
+                case "option1": //op1(); break;
+                case "option2": //op2(); break;
+            }
         }
     }
     //endregion
@@ -23,21 +26,25 @@ public class ProcedureStyleDemo {
     //region before
     private static void m1(int param) {
         if (param >= Byte.MAX_VALUE) {
-            //
+            op();
         } else {
-            //
+            //op1() --> OOP: Strategy, Template Method
         }
     }
 
     private static void m2(int param) {
         if (param >= Integer.MAX_VALUE) {
-            //
+            op();
         } else {
-            //
+            //op2() --> F: HOF
         }
     }
     //endregion
 
+    //Refactoring: Extract Method, Sprout Method
+    private static void op() {
+        //op
+    }
 
     private static boolean condition() {
         return false;
