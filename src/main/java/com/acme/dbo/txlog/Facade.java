86874c73e2707be98020166c605c31d94cb221b2
Alexey Tarasovskiy <alexey.tarasovskiy@gmail.com>
2021-03-30 14:38:27 +0300

    iteration 03

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,5 +1,7 @@
 package com.acme.dbo.txlog;
 
+import static java.lang.System.lineSeparator;
+
 public class Facade {
 
 
@@ -7,18 +9,67 @@ public class Facade {
     public static final String STRING_PREFIX = "string: ";
     private static final String OBJECT_PREFIX = "reference: ";
     private static final String CHAR_PREFIX = "char: ";
+    private static final String PRIMITIVE_ARRAY_PREFIX = "primitives array: ";
+    private static final String PRIMITIVE_MATRIX_PREFIX = "primitives matrix: ";
     private static final String PRIMITIVE_POSTFIX = "";
     private static final String STRING_POSTFIX = "";
     private static final String OBJECT_POSTFIX = "";
     private static final String CHAR_POSTFIX = "";
+    private static final String PRIMITIVE_ARRAY_POSTFIX = "";
+    private static final String PRIMITIVE_MATRIX_POSTFIX = "";
 
     private static String previousState = "NAN";
     private static String currentState = "NAN";
     private static String aggregatedString = "";
-    private static int stringAppearanceCounter =1;
+    private static int stringAppearanceCounter = 1;
     private static int aggregatedInt;
     private static int aggregatedByte;
+    private static StringBuilder aggregatedOneDimArray = new StringBuilder("");
+    private static StringBuilder aggregatedMultyDimArray = new StringBuilder("");
+    private static StringBuilder partMultyDimArray = new StringBuilder("");
 
+
+    public static void log(int[][] message) {
+        currentState = "INT[][]";
+        if (previousState.equals("INT[]") || previousState.equals("NAN") || previousState.equals("INT[][]")) {
+            //    System.out.println("INT[][] arrived:" + message);
+            aggregatedMultyDimArray.append("{" + lineSeparator());
+            for (int[] onedimArray : message) {
+            // log(onedimArray); //Has side effects!
+            //   currentState = "INT[][]"; // SETS STATE TOO, side effect! REMOVING SIDE EFFECT 1
+            //   aggregatedMultyDimArray.append(aggregatedOneDimArray).append(lineSeparator());
+            //    aggregatedOneDimArray = aggregatedOneDimArray.delete(0, aggregatedOneDimArray.length()); //REMOVING SIDE EFFECT 2
+                partMultyDimArray.append("{");
+                for (int elem : onedimArray) {
+                    partMultyDimArray.append(elem).append(", ");
+                }
+                partMultyDimArray.deleteCharAt(partMultyDimArray.length() - 1).deleteCharAt(partMultyDimArray.length() - 1);
+                partMultyDimArray.append("}");
+                aggregatedMultyDimArray.append(partMultyDimArray).append(lineSeparator());
+                partMultyDimArray = partMultyDimArray.delete(0, partMultyDimArray.length());
+            }
+            aggregatedMultyDimArray.append("}");
+        }
+        //TODO ELSE FLUSH AND STORE INT[][]
+        previousState = currentState;
+    }
+
+    public static void log(int[] message) {
+        currentState = "INT[]";
+        if (previousState.equals("INT[]") || previousState.equals("NAN") || previousState.equals("INT[][]")) {
+            //    System.out.println("INT[] arrived:" + message);
+            aggregatedOneDimArray.append("{");
+            for (int elem : message) {
+                aggregatedOneDimArray.append(elem).append(", ");
+            }
+            aggregatedOneDimArray.deleteCharAt(aggregatedOneDimArray.length() - 1).deleteCharAt(aggregatedOneDimArray.length() - 1);
+            aggregatedOneDimArray.append("}");
+        }
+        //TODO ELSE FLUSH AND STORE INT[]
+        previousState = currentState;
+    }
+
+    //TODO Refactor!
     public static void log(int message) {
         currentState = "INT";
         //    System.out.println("INT arrived:" + message);
@@ -65,12 +116,12 @@ public class Facade {
         if (previousState.equals("STR") || previousState.equals("NAN")) {
             if (aggregatedString.equals(message)) {
                 stringAppearanceCounter++;
-          //      System.out.println("#6 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#6");
+                //      System.out.println("#6 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#6");
             } else flush();
             aggregatedString = message;
-        //    System.out.println("#7 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#7");
+            //    System.out.println("#7 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#7");
         } else {
-        //    System.out.println("#8 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#8");
+            //    System.out.println("#8 " + previousState + "->" + currentState + " " + aggregatedString + " " + stringAppearanceCounter + " /#8");
             aggregatedString = message;
             flush();
         }
@@ -100,7 +151,7 @@ public class Facade {
 
     public static void flush() {
         if (previousState == "STR") {
-        //    System.out.println("#90 " + aggregatedString);
+            //    System.out.println("#90 " + aggregatedString);
             if (stringAppearanceCounter > 1) {
                 aggregatedString = aggregatedString + " (x" + stringAppearanceCounter + ")";
                 stringAppearanceCounter = 1;
@@ -115,6 +166,15 @@ public class Facade {
             //    System.out.println("#92 " + aggregatedByte);
             print(decorate(PRIMITIVE_PREFIX, aggregatedByte, PRIMITIVE_POSTFIX));
             aggregatedByte = 0;
+        } else if (previousState == "INT[]") {
+            //    System.out.println("#93 " + aggregatedInt[]);
+            print(decorate(PRIMITIVE_ARRAY_PREFIX, aggregatedOneDimArray, PRIMITIVE_ARRAY_POSTFIX));
+            aggregatedOneDimArray = aggregatedOneDimArray.delete(0, aggregatedOneDimArray.length());
+        } else if (previousState == "INT[][]") {
+            //    System.out.println("#93 " + aggregatedInt[]);
+            print(decorate(PRIMITIVE_MATRIX_PREFIX, aggregatedMultyDimArray, PRIMITIVE_MATRIX_POSTFIX));
+            aggregatedMultyDimArray = aggregatedMultyDimArray.delete(0, aggregatedMultyDimArray.length());
+            partMultyDimArray = partMultyDimArray.delete(0, partMultyDimArray.length());
         } else {
         }
     }
--- src/test/java/com/acme/dbo/txlog/iteration03/LoggerTest.java
+++ src/test/java/com/acme/dbo/txlog/iteration03/LoggerTest.java
@@ -1,10 +1,14 @@
 package com.acme.dbo.txlog.iteration03;
 
+import com.acme.dbo.txlog.Facade;
 import com.acme.dbo.txlog.SysoutCaptureAndAssertionAbility;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 import java.io.IOException;
+
+import static java.lang.System.lineSeparator;
 
 public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     //region given
@@ -20,18 +24,19 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     }
     //endregion
 
-    /*
-    TODO: implement Logger solution to match specification as tests
+
+    //  TODO: implement Logger solution to match specification as tests
 
     @Test
     public void shouldLogIntegersArray() throws IOException {
         //region when
-        Facade.log(new int[] {-1, 0, 1});
+        Facade.log(new int[]{-1, 0, 1});
         //endregion
+        Facade.flush();
 
         //region then
         assertSysoutEquals(
-            "primitives array: {-1, 0, 1}\n"
+                "primitives array: {-1, 0, 1}" + lineSeparator()
         );
         //endregion
     }
@@ -39,75 +44,78 @@ public class LoggerTest implements SysoutCaptureAndAssertionAbility {
     @Test
     public void shouldLogIntegersMatrix() throws IOException {
         //region when
-        Facade.log(new int[][] {{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}});
+        Facade.log(new int[][]{{-1, 0, 1}, {1, 2, 3}, {-1, -2, -3}});
+        Facade.flush();
         //endregion
 
         //region then
         assertSysoutEquals(
-            "primitives matrix: {\n" +
-                "{-1, 0, 1}\n" +
-                "{1, 2, 3}\n" +
-                "{-1, -2, -3}\n" +
-            "}\n"
+                "primitives matrix: {" + lineSeparator() +
+                        "{-1, 0, 1}" + lineSeparator() +
+                        "{1, 2, 3}" + lineSeparator() +
+                        "{-1, -2, -3}" + lineSeparator() +
+                        "}" + lineSeparator()
         );
         //endregion
     }
 
-    @Test
-    public void shouldLogIntegersMulitidimentionalArray() throws IOException {
-        //region when
-        Facade.log(new int[][][][] {{{{0}}}});
-        //endregion
+//    @Test
+//    public void shouldLogIntegersMulitidimentionalArray() throws IOException {
+//        //region when
+//        Facade.log(new int[][][][] {{{{0}}}});
+//        //endregion
+//
+//        //region then
+//        assertSysoutEquals(
+//            "primitives multimatrix: {\n" +
+//                "{\n" + "{\n" + "{\n" +
+//                    "0\n" +
+//                "}\n" + "}\n" + "}\n" +
+//            "}\n"
+//        );
+//        //endregion
+//    }
+//
+//    @Test
+//    public void shouldLogStringsWithOneMethodCall() throws IOException {
+//        //region when
+//        Facade.log("str1", );
+//          Facade.log("string 2");
+//          Facade.log("str 3");
+//        //endregion
+//
+//        //region then
+//        assertSysoutContains("str1\nstring 2\nstr 3");
+//        //endregion
+//    }
+//
+//    @Test
+//    public void shouldLogIntegersWithOneMethodCall() throws IOException {
+//        //region when
+//        Facade.log(-1, 0, 1, 3);
+//        //endregion
+//
+//        //region then
+//        assertSysoutContains("3");
+//        //endregion
+//    }
+//
+//    @Test
+//    public void shouldCorrectDealWithIntegerOverflowWhenOneMethodCall() throws IOException {
+//        //region when
+//        Facade.log(1);
+//        Facade.log("str");
+//        Facade.log(Integer.MAX_VALUE - 10);
+//        Facade.log(11);
+//        //endregion
+//
+//        //region then
+//        assertSysoutContains(1);
+//        assertSysoutContains("str");
+//        assertSysoutContains(Integer.MAX_VALUE - 10);
+//        assertSysoutContains(11);
+//        //endregion
+//    }
 
-        //region then
-        assertSysoutEquals(
-            "primitives multimatrix: {\n" +
-                "{\n" + "{\n" + "{\n" +
-                    "0\n" +
-                "}\n" + "}\n" + "}\n" +
-            "}\n"
-        );
-        //endregion
-    }
 
-    @Test
-    public void shouldLogStringsWithOneMethodCall() throws IOException {
-        //region when
-        Facade.log("str1", "string 2", "str 3");
-        //endregion
-
-        //region then
-        assertSysoutContains("str1\nstring 2\nstr 3");
-        //endregion
-    }
-
-    @Test
-    public void shouldLogIntegersWithOneMethodCall() throws IOException {
-        //region when
-        Facade.log(-1, 0, 1, 3);
-        //endregion
-
-        //region then
-        assertSysoutContains("3");
-        //endregion
-    }
-
-    @Test
-    public void shouldCorrectDealWithIntegerOverflowWhenOneMethodCall() throws IOException {
-        //region when
-        Facade.log(1);
-        Facade.log("str");
-        Facade.log(Integer.MAX_VALUE - 10);
-        Facade.log(11);
-        //endregion
-
-        //region then
-        assertSysoutContains(1);
-        assertSysoutContains("str");
-        assertSysoutContains(Integer.MAX_VALUE - 10);
-        assertSysoutContains(11);
-        //endregion
-    }
-
-    */
 }
