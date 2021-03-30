f025361aa9ea4b449dea310d2db86d15145718a4
alex.samarkin <alexgui@yandex.ru>
2021-03-30 15:38:00 +0300

    refactor

--- src/main/java/com/acme/dbo/txlog/ArrayToStringFormatter.java
+++ src/main/java/com/acme/dbo/txlog/ArrayToStringFormatter.java
@@ -4,18 +4,17 @@ import java.util.Arrays;
 import java.util.stream.Collectors;
 
 public class ArrayToStringFormatter implements Format {
-
     private static String LEFT_BRACKET = "{";
     private static String RIGHT_BRACKET = "}";
     private static String ARRAY_DIVIDER = ", ";
 
-     public String format(int[] array) {
+    public String format(int[] array) {
         return LEFT_BRACKET + Arrays.stream(array)
                 .mapToObj(String::valueOf)
                 .collect(Collectors.joining(ARRAY_DIVIDER)) + RIGHT_BRACKET;
     }
 
-     public String format(int[][] array) {
+    public String format(int[][] array) {
         String result = LEFT_BRACKET + System.lineSeparator();
         for (int i = 0; i < array.length; ++i)
         {
