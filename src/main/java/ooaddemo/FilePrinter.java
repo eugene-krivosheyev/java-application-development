1e110e14cc7d839ad25bf32e706e97b2b053ab2e
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-01 11:16:15 +0300

    Add polymorphism with refactoring to OOAD.

--- src/main/java/ooaddemo/FilePrinter.java
+++ src/main/java/ooaddemo/FilePrinter.java
@@ -1,12 +1,13 @@
 package ooaddemo;
 
-public class FilePrinter {
+public class FilePrinter implements Printer {
     private String file;
 
     public FilePrinter(String file) {
         this.file = file;
     }
 
+    @Override
     public void print(String message) {
         System.out.println("to file: " + file + " > " + message);
     }
--- src/main/java/ooaddemo/SeverityMessageFilter.java
+++ src/main/java/ooaddemo/SeverityMessageFilter.java
@@ -1,12 +1,13 @@
 package ooaddemo;
 
-public class SeverityMessageFilter {
+public class SeverityMessageFilter implements MessageFilter {
     private final SeverityLevel threshold;
 
     public SeverityMessageFilter(SeverityLevel threshold) {
         this.threshold = threshold;
     }
 
+    @Override
     public boolean filter(String message, SeverityLevel severity) {
         return severity.compareTo(threshold) < 0;
     }
