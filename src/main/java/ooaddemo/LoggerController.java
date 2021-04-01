d1993c553d67339da0473f28a5a30ffa819717f5
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-01 11:17:05 +0300

    Add Creator DP with refactoring to OOAD.

--- src/main/java/ooaddemo/LoggerController.java
+++ src/main/java/ooaddemo/LoggerController.java
@@ -1,7 +1,7 @@
 package ooaddemo;
 
 public class LoggerController {
-    private final Printer printer = new FilePrinter("out.txt");
+    private final Printer printer = new FilePrinter("out.txt"); //Creator [GRASP]
     private final MessageFilter filter = new SeverityMessageFilter(SeverityLevel.WARNING);
 
     public void log(String message, SeverityLevel severity) {
