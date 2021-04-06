a002ac051bb614c018911356f99c8232774b8bfb
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-06 12:03:11 +0300

    Add encapsulation demo.

--- src/main/java/ooaddemo/message/StringMessage.java
+++ src/main/java/ooaddemo/message/StringMessage.java
@@ -7,6 +7,18 @@ public class StringMessage implements DecoratingMessage {
         this.body = body;
     }
 
+    /**
+     * JavaBeans -> POJO
+     * OCP
+     */
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
     @Override
     public String getDecoratedMessage() {
         return "string: " + body;
