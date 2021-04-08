5d9cc651721f9973d05257a0710e76dd7890c1b2
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-08 13:49:46 +0300

    Add constructors demo.

---
+++ src/main/java/demo/InstatiationDemo.java
@@ -0,0 +1,38 @@
+package demo;
+
+public class InstatiationDemo {
+    public static void main(String[] args) {
+        int formak = 1;
+        IntMessage message = new IntMessage(1);
+        IntMessage message2 = new IntMessage();
+    }
+}
+
+/**
+ * Immutable
+ */
+class IntMessage {
+    private int body;
+
+    /**
+     * overloafing
+     */
+    public IntMessage() {
+        this(0); //
+    }
+
+    {
+        System.out.println();
+        System.out.println();
+        System.out.println();
+        System.out.println();
+    }
+
+    IntMessage(final int body) {
+        this.body = body;
+    }
+
+    public int getBody() {
+        return this.body;
+    }
+}
