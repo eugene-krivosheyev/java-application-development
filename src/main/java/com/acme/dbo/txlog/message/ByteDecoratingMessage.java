707049e510755fb1f03d470f586fb72c13a1a2d7
rmatafonov <matafonov.rv@gmail.com>
2021-04-10 08:16:05 +0300

    Extracted NumberDecoratingMessage for byte and int

--- src/main/java/com/acme/dbo/txlog/message/ByteDecoratingMessage.java
+++ src/main/java/com/acme/dbo/txlog/message/ByteDecoratingMessage.java
@@ -1,30 +1,8 @@
 package com.acme.dbo.txlog.message;
 
-public class ByteDecoratingMessage implements DecoratingMessage {
-    private final long body;
-
+public class ByteDecoratingMessage extends NumberDecoratingMessage {
     public ByteDecoratingMessage(final long body) {
-        this.body = body;
-    }
-
-    @Override
-    public String getDecoratedMessage() {
-        return "primitive: " + body;
-    }
-
-    @Override
-    public DecoratingMessage accumulate(final DecoratingMessage message) {
-        if (!(message instanceof ByteDecoratingMessage)) {
-            throw new IllegalArgumentException("Parameter 'message' is not of type " + this.getClass().getTypeName());
-        }
-        final ByteDecoratingMessage addingMessage = (ByteDecoratingMessage) message;
-        if (body + addingMessage.getBody() > Byte.MAX_VALUE) {
-            return message;
-        }
-        return new ByteDecoratingMessage(body + addingMessage.getBody());
-    }
-
-    public long getBody() {
-        return this.body;
+        super(body);
+        this.maxValue = Byte.MAX_VALUE;
     }
 }
--- src/main/java/com/acme/dbo/txlog/message/IntDecoratingMessage.java
+++ src/main/java/com/acme/dbo/txlog/message/IntDecoratingMessage.java
@@ -1,30 +1,8 @@
 package com.acme.dbo.txlog.message;
 
-public class IntDecoratingMessage implements DecoratingMessage {
-    private final long body;
-
+public class IntDecoratingMessage extends NumberDecoratingMessage {
     public IntDecoratingMessage(final long body) {
-        this.body = body;
-    }
-
-    @Override
-    public String getDecoratedMessage() {
-        return "primitive: " + body;
-    }
-
-    @Override
-    public DecoratingMessage accumulate(final DecoratingMessage message) {
-        if (!(message instanceof IntDecoratingMessage)) {
-            throw new IllegalArgumentException("Parameter 'message' is not of type " + this.getClass().getTypeName());
-        }
-        final IntDecoratingMessage addingMessage = (IntDecoratingMessage) message;
-        if (body + addingMessage.getBody() > Integer.MAX_VALUE) {
-            return message;
-        }
-        return new IntDecoratingMessage(body + addingMessage.getBody());
-    }
-
-    public long getBody() {
-        return this.body;
+        super(body);
+        this.maxValue = Integer.MAX_VALUE;
     }
 }
