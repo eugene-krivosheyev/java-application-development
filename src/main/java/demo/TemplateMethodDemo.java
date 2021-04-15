e90a80f44e5f9271ab49cd875e67bff931968914
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-15 13:43:06 +0300

    Add Template Method DP demo.

--- src/main/java/demo/TemplateMethodDemo.java
+++ src/main/java/demo/TemplateMethodDemo.java
@@ -4,32 +4,33 @@ import ooadrefactoring.Message;
 
 public class TemplateMethodDemo {
     public static void main(String[] args) {
-
+        Logger logger = new LoggerImpl1();
+        logger.log(null);
     }
 }
 
-class Logger {
-    public void log(Message m, key) {
+abstract class Logger {
+    public void log(Message m) {
         //....
         //....
-        strategy.step(); //strategy [GoF]
+        this.step(); //Template Method [GoF]
         //....
         //....
     }
 
-    public void log(int m) {
-        //.....
-        //.....
-        step1();
-        //.....
-        //.....
-    }
+    protected abstract void step();
+}
 
-    public void log(String m) {
-        //.....
-        //.....
-        step2();
-        //.....
-        //.....
+class LoggerImpl1 extends Logger {
+    @Override
+    protected void step() {
+        System.out.println("1");
+    }
+}
+
+class LoggerImpl2 extends Logger {
+    @Override
+    protected void step() {
+        System.out.println("2");
     }
 }
