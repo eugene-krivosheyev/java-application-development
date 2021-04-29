bd0527326bba04c1c6a90aa330a31f1f8ae71e2d
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-29 09:53:30 +0300

    Add file IO demo.

--- src/main/java/eappdemo/Framework.java
+++ src/main/java/eappdemo/Framework.java
@@ -1,9 +1,24 @@
 package eappdemo;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class Framework {
     public static void main(String[] args) {
         //region DI Core
         //xml config | @Annotations
+
+        try {
+            final List<String> strings = Files.readAllLines(Paths.get("file.txt"));
+            final Stream<String> lines = Files.lines(Paths.get("file.txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         AccountController controller = new AccountController(
                 new AccountService(new AccountRepository())
         );
