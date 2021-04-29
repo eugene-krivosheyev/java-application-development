c533818f6cb4bdc35bfbbe3a50769f1330e61491
Eugene Krivosheyev <eugene.krivosheyev@gmail.com>
2021-04-29 11:51:45 +0300

    Add thread sync demo.

--- src/main/java/eappdemo/AccountController.java
+++ src/main/java/eappdemo/AccountController.java
@@ -5,6 +5,8 @@ package eappdemo;
  */
 public class AccountController {
     private AccountService accountService;
+    volatile int counter = 0;
+    private Object monitor = new Object();
 
     public AccountController(AccountService accountService) {
         this.accountService = accountService;
@@ -12,6 +14,12 @@ public class AccountController {
 
     //Config: external xml | @GetRequest("/path")
     public void findAccountById(FindRequest request) {
+        synchronized (monitor) { //Lock -> ReadWriteLock
+            counter++; //1,2,3
+        } // -> AtomicXXX: non-blocking
+
+        if (counter == 5) counter = 0;
+
         int i = 0;
         if (request.getId() == null) throw new IllegalArgumentException("id null");
         if (request.getId() < 0) throw new IllegalArgumentException("id negative");
