2bef0c049136c447b10935aa5a001bce0a374cb8
Artem Izikaev <aizikaev@gmail.com>
2022-04-12 09:18:51 -0400

    practice: Hand-on Iteration 01: Decorating Transaction Logger

--- src/main/java/com/acme/dbo/txlog/Facade.java
+++ src/main/java/com/acme/dbo/txlog/Facade.java
@@ -1,11 +1,28 @@
 package com.acme.dbo.txlog;
 
+import com.acme.dbo.txlog.printer.ConsolePrinter;
+
+import static com.acme.dbo.txlog.decorator.MessageDecorator.decorate;
+import static com.acme.dbo.txlog.printer.ConsolePrinter.*;
+
 public class Facade {
     public static void log(int message) {
-        System.out.println("primitive: " + message);
+        print(decorate(message));
     }
 
-    public static void log(byte message) {
-        System.out.println("primitive: " + message);
+    public static void log(char message) {
+        print(decorate(message));
+    }
+
+    public static void log(String message) {
+        print(decorate(message));
+    }
+
+    public static void log(boolean message) {
+        print(decorate(message));
+    }
+
+    public static void log(Object message) {
+        print(decorate(message));
     }
 }
